26dd26766cf4f8af177617012b499b018c118de6
Sam Wight <samuelwight@gmail.com>
2020-05-02 15:18:58 -0500

    Add background image with breakpoints.

--- src/components/home/MainHeader.tsx
+++ src/components/home/MainHeader.tsx
@@ -5,7 +5,7 @@ import BackgroundImage from 'gatsby-background-image'
 
 const query = graphql`
 	query {
-		shelby: file(relativePath: { eq: "backdrop.png" }) {
+		shelby: file(relativePath: { eq: "shelby-dark.png" }) {
 			childImageSharp {
 				fluid(maxWidth: 1000) {
 					...GatsbyImageSharpFluid_noBase64
@@ -19,26 +19,32 @@ const MainHeader = () => {
 	const data = useStaticQuery(query)
 
 	return (
-		<div className='py-40 dark:bg-gray-900'>
-			<div className='container mx-auto px-5'>
-				<div className='max-w-5xl'>
-					<h1 className='text-5xl lg:text-6xl tracking-normal leading-tight lg:leading-normal font-bold text-black dark:text-white'>
-						We build tech for nonprofits.
-					</h1>
-					<p className='text-xl text-black dark:text-gray-200'>
-						We're a group of students at The University of Alabama who build
-						technology solutions free-of-charge for nonprofits and those who do
-						good.
-					</p>
-					<Link
-						to='/about'
-						className='inline-block font-light bg-primary transition-all duration-300 shadow-lg hover:shadow-xl hover:bg-primary-light my-5 text-white rounded-md text-lg px-8 py-3'
-					>
-						Learn more
-					</Link>
+		<BackgroundImage
+			Tag='div'
+			fluid={[data.shelby.childImageSharp.fluid]}
+			className='shelby-background'
+		>
+			<div className='shelby-image py-40 dark:bg-gray-900'>
+				<div className='container mx-auto px-5'>
+					<div className='max-w-5xl'>
+						<h1 className='text-5xl lg:text-6xl tracking-normal leading-tight lg:leading-normal font-bold text-black dark:text-white'>
+							We build tech for nonprofits.
+						</h1>
+						<p className='text-xl text-black dark:text-gray-200 max-w-lg lg:max-w-3xl'>
+							We're a group of students at The University of Alabama who build
+							technology solutions free-of-charge for nonprofits and those who
+							do good.
+						</p>
+						<Link
+							to='/about'
+							className='inline-block font-light bg-primary transition-all duration-300 shadow-lg hover:shadow-xl hover:bg-primary-light my-5 text-white rounded-md text-lg px-8 py-3'
+						>
+							Learn more
+						</Link>
+					</div>
 				</div>
 			</div>
-		</div>
+		</BackgroundImage>
 	)
 
 	// return (
