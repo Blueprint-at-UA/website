5edc5ba7113e5495f3e0b7a9dc6dbd4e3163f78a
Sam Wight <samuelwight@gmail.com>
2020-05-02 13:11:38 -0500

    Add dark mode and dark theme.

--- src/components/home/AboutPiece.tsx
+++ src/components/home/AboutPiece.tsx
@@ -5,20 +5,20 @@ import { Button } from 'react-bootstrap'
 
 const AboutPiece = () => {
 	return (
-		<div className='py-20 bg-gray-100 px-5'>
+		<div className='py-20 bg-gray-100 px-5 dark:bg-primary'>
 			<div className='container mx-auto'>
-				<h1 className='text-gray-700 text-2xl'>
+				<h1 className='text-gray-700 text-2xl dark:text-white'>
 					Blueprint at UA strives to make technology accessible for those who
 					assist communities and promote public welfare.
 				</h1>
-				<p className='mt-3'>
+				<p className='mt-3 dark:text-gray-200'>
 					We're a student organization at The University of Alabama committed to
 					working with nonprofits, big and small, to take on their problems with
 					modern solutions. Each semester, we create teams of 4 to 5 talented
 					students and use their combined skill to fix nonprofits' problems with
 					solutions through tech.
 				</p>
-				<p className='mt-2'>
+				<p className='mt-2 dark:text-gray-200'>
 					Over the course of a project's lifespan, teams work with their
 					assigned nonprofits to make sure all software built is well-designed,
 					performant, and cheap to deploy. We never charge for any of our
