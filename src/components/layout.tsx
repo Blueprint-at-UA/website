5f3eb32f737859fd9fccffbc7b1cce530632b9cc
Sam Wight <samuelwight@gmail.com>
2020-05-01 20:47:47 -0500

    Changed.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -9,7 +9,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { useStaticQuery, graphql } from 'gatsby'
 
-import Navbar from './layout/header/Navbar'
+import Navbar from './layout/Navbar'
 import './layout/layout.css'
 
 const Layout: React.FC = ({ children }) => {
